84ab0745942c56781cf1db52fe16d5070091816e
Léo Gaspard <leo@gaspard.io>
2025-01-09 21:58:08 +0100

    handle lints

--- custom_components/acer_projector2/__init__.py
+++ custom_components/acer_projector2/__init__.py
@@ -2,23 +2,27 @@
 
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry
+from typing import TYPE_CHECKING
+
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
+
+if TYPE_CHECKING:
+    from homeassistant.config_entries import ConfigEntry
+    from homeassistant.core import HomeAssistant
 
 PLATFORMS: list[Platform] = [Platform.SWITCH]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Setup this integration using the UI"""
+    """Set up this integration using the UI."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Handle removal of an entry"""
+    """Handle removal of an entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle reload of an entry"""
+    """Handle reload of an entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
