cc7e7e02fa0ced83843891406e8676730ef703e6
Léo Gaspard <leo@gaspard.io>
2025-01-09 22:13:08 +0100

    reformat

--- custom_components/acer_projector2/__init__.py
+++ custom_components/acer_projector2/__init__.py
@@ -12,15 +12,18 @@ if TYPE_CHECKING:
 
 PLATFORMS: list[Platform] = [Platform.SWITCH]
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up this integration using the UI."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Handle removal of an entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle reload of an entry."""
